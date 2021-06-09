19ed04a17d7d671967db7cdd8f200a9d491064e7
znnrz <norz914@gmail.com>
2021-06-09 10:28:55 +0800

    Update JavaOutputFormatting.java

--- JavaOutputFormatting.java
+++ JavaOutputFormatting.java
@@ -12,15 +12,15 @@ public class JavaOutputFormatting {
             String str = scan.next();
             int num = scan.nextInt();
 
-            // Kondisi int hanya bisa 0-999
+           // Kondisi int hanya bisa 0-999
             if (num >= 999) {
-                String convNum = String.valueOf(num);
-                convNum = convNum.substring(0, 3);
-                num = Integer.parseInt(convNum);
+                String convNum = String.valueOf(num); // Konversi int ke string
+                convNum = convNum.substring(0, 3); // Memotong hasil hanya menampilkan 3 karakter
+                num = Integer.parseInt(convNum); // Konversi string ke int
 
                 // Kondisi string hanya bisa 10 karakter
                 if (str.length() > 10) {
-                    str = str.substring(0, 10);
+                    str = str.substring(0, 10); // Memotong hasil hanya menampilkan 10 karakter
                 }
             }
                 // Mencetak hasil dengan format
